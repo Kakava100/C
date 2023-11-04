3fb1d87414cacda78291c862effed1f9efb6caaa
Kakava100 <149315730+Kakava100@users.noreply.github.com>
2023-11-05 01:00:50 +0300

    Add files via upload

---
+++ comb.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf(" _   _\n");
+	printf("/ \\_/ \\\n");
+	printf("\\_/ \\_/\n");
+	printf("/ \\_/ \\\n");
+	printf("\\_/ \\_/\n");
+
+	return 0;
+}
+
